a35bca33d2537110b4824ef8ef56f851dcd17ed5
Thomas Apodaca <tma@angrymonkey.com>
2015-03-26 16:58:56 -0700

    database config

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
