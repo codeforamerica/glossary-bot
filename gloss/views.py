8720bc3a4033f0dabf64c9a31cc3b7bf5b9730fd
Thomas Apodaca <tma@angrymonkey.com>
2015-03-27 14:26:09 -0700

    basic authorize functionality

--- gloss/views.py
+++ gloss/views.py
@@ -1,6 +1,11 @@
 from . import gloss as app
 from . import db
+from flask import abort, current_app, request
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
-    return 'Glossary Bot Reporting for Duty!'
+    # verify that the request is authorized
+    if request.form['token'] != current_app.config['SLACK_TOKEN']:
+        abort(401)
+
+    return 'you are authorized!', 200
