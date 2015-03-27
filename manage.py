c063bf21c187c7763a90f387ea9c218aaae28ff4
Thomas Apodaca <tma@angrymonkey.com>
2015-03-27 16:59:11 -0700

    parsing environment variables from a local .env file

--- manage.py
+++ manage.py
@@ -1,7 +1,14 @@
-from os import environ
+from os import environ, path
 from gloss import create_app, db
 from gloss.models import Definition
 from flask.ext.script import Manager, Shell
+
+# grab environment variables from the .env file if it exists
+if path.exists('.env'):
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            environ[var[0]] = var[1]
 
 app = create_app(environ)
 manager = Manager(app)
