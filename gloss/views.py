b55131ad2a88c408f9599b23305100751499ab61
Thomas Apodaca <tma@angrymonkey.com>
2015-03-27 14:57:06 -0700

    repeating parameters back to requestor, for testing

--- gloss/views.py
+++ gloss/views.py
@@ -8,4 +8,6 @@ def index():
     if request.form['token'] != current_app.config['SLACK_TOKEN']:
         abort(401)
 
-    return 'you are authorized!', 200
+    params = {'team_id': request.form['team_id'], 'team_domain': request.form['team_domain'], 'channel_id': request.form['channel_id'], 'channel_name': request.form['channel_name'], 'user_id': request.form['user_id'], 'user_name': request.form['user_name'], 'command': request.form['command'], 'text': request.form['text']}
+
+    return 'you are authorized, and you said {text}! team_id:{team_id} team_domain:{team_domain} channel_id:{channel_id} channel_name:{channel_name} user_id:{user_id} user_name:{user_name} command:{command}'.format(**params), 200
