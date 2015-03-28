d4b92465a876dcc63fba30a8e5ab638e947d14fd
Thomas Apodaca <tma@angrymonkey.com>
2015-03-28 01:19:02 -0700

    reordered

--- gloss/views.py
+++ gloss/views.py
@@ -55,9 +55,6 @@ def index():
     if command_action == u'help':
         return 'I think you want help using Glossary Bot!', 200
 
-    # they asked for a definition
-    return 'I think you want a definiton for the term "{}"'.format(full_text), 200
-
     # send the definition to the webhook
     payload_template = '''{"channel": "#{channel_name}", "username": "{username}", "text": "{text}", "icon_emoji": ":{icon_emoji}:"}'''
     payload_values = {}
@@ -68,6 +65,8 @@ def index():
     payload = payload_template.format(**payload_values)
     webhook_response = post(current_app.config['SLACK_WEBHOOK_URL'], data=payload)
 
+    # they asked for a definition
+    return 'I think you want a definiton for the term "{}". Response from the webhook to #{}: {}'.format(full_text, unicode(request.form['channel_name']), webhook_response.status_code), 200
 
     # params = {'team_id': request.form['team_id'], 'team_domain': request.form['team_domain'], 'channel_id': request.form['channel_id'], 'channel_name': request.form['channel_name'], 'user_id': request.form['user_id'], 'user_name': request.form['user_name'], 'command': request.form['command'], 'text': request.form['text']}
     # return 'you are authorized, and you said {text}! team_id:{team_id} team_domain:{team_domain} channel_id:{channel_id} channel_name:{channel_name} user_id:{user_id} user_name:{user_name} command:{command}'.format(**params), 200
