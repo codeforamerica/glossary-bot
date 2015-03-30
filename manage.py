358a0a7dbbf5ae56cedeb94a46223d66544cffc5
Thomas Apodaca <tma@angrymonkey.com>
2015-03-30 13:10:06 -0700

    added manager commands for dropping and creating database

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from os import environ, path
 from gloss import create_app, db
 from gloss.models import Definition, Interaction
-from flask.ext.script import Manager
+from flask.ext.script import Manager, prompt_bool
 
 # grab environment variables from the .env file if it exists
 if path.exists('.env'):
@@ -16,6 +16,15 @@ manager = Manager(app)
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=db, Definition=Definition, Interaction=Interaction)
+
+@manager.command
+def dropdb():
+    if prompt_bool("Are you sure you want to lose all your data?"):
+        db.drop_all()
+
+@manager.command
+def createdb():
+    db.create_all()
 
 if __name__ == '__main__':
     manager.run()
