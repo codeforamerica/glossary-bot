39dc2aa096d7168aeab5839305fc7bb283262a8a
Thomas Apodaca <tma@angrymonkey.com>
2015-03-30 11:46:34 -0700

    using shell decorator insteady of add_command

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from os import environ, path
 from gloss import create_app, db
 from gloss.models import Definition, Interaction
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager
 
 # grab environment variables from the .env file if it exists
 if path.exists('.env'):
@@ -13,9 +13,9 @@ if path.exists('.env'):
 app = create_app(environ)
 manager = Manager(app)
 
+@manager.shell
 def make_shell_context():
     return dict(app=app, db=db, Definition=Definition, Interaction=Interaction)
-manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
