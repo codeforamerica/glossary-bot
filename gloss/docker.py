4f4d78e46f470a8f8bd1d61034a8895a3510b0d7
Thomas Apodaca <tma@angrymonkey.com>
2015-05-05 10:35:47 -0700

    fixed indentation

--- gloss/docker.py
+++ gloss/docker.py
@@ -2,14 +2,15 @@ from os import environ
 from . import create_app
 
 if 'DATABASE_URL' not in environ:
-  host = environ.get('DB_PORT_5432_TCP_ADDR', '')
-  port = environ.get('DB_PORT_5432_TCP_PORT', '')
-  db = environ.get('DB_DATABASENAME', '')
+    host = environ.get('DB_PORT_5432_TCP_ADDR', '')
+    port = environ.get('DB_PORT_5432_TCP_PORT', '')
+    db = environ.get('DB_DATABASENAME', '')
 
-  pswd = environ.get('DB_PASS', '')
-  if pswd != '': pswd = ":" + pswd
+    pswd = environ.get('DB_PASS', '')
+    if pswd != '':
+        pswd = ":" + pswd
 
-  environ['DATABASE_URL'] = "postgresql://postgres%s@%s/%s" % (pswd, host, db)
+    environ['DATABASE_URL'] = "postgresql://postgres%s@%s/%s" % (pswd, host, db)
 
 print environ['DATABASE_URL']
 
