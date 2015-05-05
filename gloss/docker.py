e0755baf402d9dc009da46b2677241838ed8755a
Thomas Apodaca <tma@angrymonkey.com>
2015-05-05 13:20:33 -0700

    cleanup

--- gloss/docker.py
+++ gloss/docker.py
@@ -4,13 +4,11 @@ from . import create_app
 if 'DATABASE_URL' not in environ:
     host = environ.get('DB_PORT_5432_TCP_ADDR', '')
     port = environ.get('DB_PORT_5432_TCP_PORT', '')
-    db = environ.get('DB_DATABASENAME', '')
-
+    name = environ.get('DB_DATABASENAME', '')
     pswd = environ.get('DB_PASS', '')
-    if pswd != '':
-        pswd = ":" + pswd
+    pswd = ":{}".format(pswd) if pswd else ''
 
-    environ['DATABASE_URL'] = "postgresql://postgres%s@%s/%s" % (pswd, host, db)
+    environ['DATABASE_URL'] = "postgresql://postgres{pswd}@{host}/{name}".format(pswd=pswd, host=host, name=name)
 
 print environ['DATABASE_URL']
 
